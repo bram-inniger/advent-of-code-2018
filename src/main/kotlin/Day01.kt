72517e6d42472dc58cdc5ffc1c311efedee84579
Bram Inniger <bram@inniger.be>
2018-12-03 14:22:28 +0200

    D01P02 - Simplified logic and made code more compact

--- src/main/kotlin/Day01.kt
+++ src/main/kotlin/Day01.kt
@@ -8,18 +8,13 @@ class Day01 {
     fun solveFirst(changes: List<String>) = changes.map(String::toInt).sum()
 
     fun solveSecond(changes: List<String>): Int {
-        return solveSecondRec(changes.map(String::toInt), 0, HashTreePSet.empty(), 0)
+        return findRepeatedFrequency(changes.map(String::toInt), 0, HashTreePSet.empty())
     }
 
-    private tailrec fun solveSecondRec(
-        changes: List<Int>, frequency: Int, frequenciesSeen: PSet<Int>, counter: Int
-    ): Int {
-        return if (frequenciesSeen.contains(frequency)) {
+    private tailrec fun findRepeatedFrequency(changes: List<Int>, frequency: Int, reached: PSet<Int>): Int =
+        if (reached.contains(frequency)) {
             frequency
         } else {
-            solveSecondRec(
-                changes, frequency + changes[counter % changes.size], frequenciesSeen.plus(frequency), counter + 1
-            )
+            findRepeatedFrequency(changes, frequency + changes[reached.size % changes.size], reached.plus(frequency))
         }
-    }
 }
