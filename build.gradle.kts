eba61d16eca50c0e60ddf0a1dda28ac12ac33481
Bram Inniger <bram@inniger.be>
2018-12-03 10:56:43 +0200

    Project structure bootstrap

---
+++ build.gradle.kts
@@ -0,0 +1,26 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.3.10"
+}
+
+group = "be.inniger"
+version = "1.0-SNAPSHOT"
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile(kotlin("test-junit"))
+}
+
+repositories {
+    jcenter()
+}
+
+tasks.withType<Wrapper> {
+    gradleVersion = "5.0"
+    Wrapper.DistributionType.ALL
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
