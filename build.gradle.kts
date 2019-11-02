af978393e67dd85c19c0c2b0f2dfc947eaa54bad
Bram Inniger <bram@inniger.be>
2019-11-02 20:51:01 +0100

    Updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,21 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.3.50"
 }
 
 group = "be.inniger"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation(group = "io.vavr", name = "vavr-kotlin", version = "0.9.2")
+    implementation(group = "io.vavr", name = "vavr-kotlin", version = "0.10.2")
     testImplementation(kotlin("test-junit"))
 }
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions.jvmTarget = "1.8"
-
-repositories {
-    jcenter()
-}
-
-tasks.withType<Wrapper> {
-    gradleVersion = "5.0"
+tasks.wrapper {
     Wrapper.DistributionType.ALL
 }
+
